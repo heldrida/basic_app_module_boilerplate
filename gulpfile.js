1b91ededbf48387c4ad0fc373e8cf626f162ce1d
Helder <helder@lamoulade.com>
2016-07-20 12:03:31 +0100

    Unit tester, test runner installed; Updated the npm, gulpfile, etc

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,49 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     plumber = require('gulp-plumber'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    jasmine = require('gulp-jasmine'),
+	KarmaServer = require('karma').Server,
+	rename = require('gulp-rename'),
+	webpack = require('webpack'),
+	webpackConfig = require('./webpack.config.js'),
+	gulpWebpack = require('gulp-webpack');
+
+gulp.task("webpack", function() {
+    return gulp.src('./src/js/index.js')
+    .pipe(rename('bundle.js'))
+    .pipe( gulpWebpack(webpackConfig, webpack) )
+    .pipe(gulp.dest('./dist/js/'));
+});
 
 gulp.task('sass', function() {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('./src/sass/app.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream({match: '**/*.css'}));
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
     gulp.watch("./**/*.html", ['reload']);
-    gulp.watch("./js/**/*.js", ['reload']);
+    gulp.watch("./src/js/**/*.js", ['reload']);
+	gulp.watch('./src/**/*.js', ['testRunner']);
+	gulp.watch('./src/**/*.js', ['webpack']);
 });
 
 gulp.task('reload', function() {
     browserSync.reload();
 });
 
+gulp.task('testRunner', function (done) {
+  new KarmaServer({
+    configFile: __dirname +  '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
 
 gulp.task('serve', ['watch'], function() {
 
---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+function App (val) {
+
+	console.log('App initialised!');
+
+	this.calc = function (val) {
+		return val * 2;
+	}
+
+}
+
+module.exports = App;
